d1de5922328f7cfdc5d5557527a94fed136f6bc5
nguyenviet3057 <nguyenviet3057@gmail.com>
2023-12-13 21:37:04 +0700

    add checkin function for time keeping

--- Controllers/TimeKeepingController.cs
+++ Controllers/TimeKeepingController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DoAnKy3.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +15,77 @@ namespace DoAnKy3.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetData()
+        {
+            USER user = ValidateUser();
+            if (user == null) return Json(StatusUnauthorized());
+
+            ResponseModel response = new ResponseModel();
+            try
+            {
+                var time_keeps = db.TIME_KEEPs
+                    .Where(o => o.EMP_NUM == user.USER_ID)
+                    .Select(o => new
+                    {
+                        TIME_DATE = o.TIME_DATE.Month + "/" + o.TIME_DATE.Day + "/" + o.TIME_DATE.Year,
+                        o.TIME_CLK_IN,
+                        o.TIME_CLK_OUT,
+                        o.TIME_ABST
+                    })
+                    .ToList();
+
+                response.status = ResponseModel.StatusCode.Success;
+                response.data = JsonConvert.SerializeObject(time_keeps);
+                response.message = "Get time keeping data successfully!";
+                return Json(response);
+            }
+            catch
+            {
+                response.status = ResponseModel.StatusCode.Error;
+                response.message = "Error";
+                return Json(response);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Checkin()
+        {
+            USER user = ValidateUser();
+            if (user == null) return Json(StatusUnauthorized());
+
+            ResponseModel response = new ResponseModel();
+            try
+            {
+                TIME_KEEP time_keep = new TIME_KEEP();
+
+                var now = DateTime.Now;
+                time_keep.TIME_CODE = now.Year.ToString() + now.Month.ToString() + now.Day.ToString() + "-" + user.USER_ID.ToString();
+                time_keep.EMP_NUM = user.USER_ID;
+                time_keep.TIME_DATE = now;
+                time_keep.TIME_CLK_IN = new TimeSpan(0, now.TimeOfDay.Hours, now.TimeOfDay.Minutes, now.TimeOfDay.Seconds);
+                time_keep.TIME_ABST = 1;
+                db.TIME_KEEPs.InsertOnSubmit(time_keep);
+
+                db.SubmitChanges();
+
+                dynamic data = new ExpandoObject();
+                data.TIME_DATE = time_keep.TIME_DATE.Month + "/" + time_keep.TIME_DATE.Day + "/" + time_keep.TIME_DATE.Year;
+                data.TIME_CLK_IN = time_keep.TIME_CLK_IN;
+
+                response.status = ResponseModel.StatusCode.Success;
+                response.data = JsonConvert.SerializeObject(data);
+                response.message = "Check-in successfully!";
+                return Json(response);
+            }
+            catch
+            {
+                response.status = ResponseModel.StatusCode.Error;
+                response.message = "Error";
+                return Json(response);
+            }
         }
 
         // GET: TimeKeeping/Details/5
